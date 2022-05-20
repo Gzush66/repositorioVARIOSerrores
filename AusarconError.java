ed756c4d3456fa26faf8b539c6c286fbe08d2971
Juan Jesus Lozano <garbagetin1@gmail.com>
2022-05-20 17:49:42 +0200

    version sin repasar

---
+++ AusarconError.java
@@ -0,0 +1,12 @@
+package Ejercicios;
+
+public class tareaClase00202 {
+    System.out.println("hola mundo");
+    System.out.println("solo un archivo para usar en practicas de GIT");
+
+
+
+
+}
+
+si borras esta linea desaparece el error
