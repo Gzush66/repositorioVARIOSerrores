fc4618e9ba75cb9e9abe9c87aaee6475f822fff6
Juan Jesus Lozano <garbagetin1@gmail.com>
2022-05-20 18:11:37 +0200

    repasado dos

--- AusarconError.java
+++ AusarconError.java
@@ -4,9 +4,9 @@ public class tareaClase00202 {
     System.out.println("hola mundo");
     System.out.println("solo un archivo para usar en practicas de GIT");
 
-
+System.out.println(" te he borrado la linea que te daba error...");
 
 
 }
 
-si borras esta linea desaparece el error
+
